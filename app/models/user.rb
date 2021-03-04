6c6d993953439e590ecff4aefc39424a1d889a74
sirine <sirine18mhedhbi@gmail.com>
2021-03-04 14:10:07 +0100

    CHORE-config-letter-opener

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ActiveRecord::Base
 
   extend Devise::Models #added this line to extend devise model
 # Include default devise modules. Others available are:
-# :confirmable, :lockable, :timeoutable and :omniauthable
-devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
+devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
+
 include DeviseTokenAuth::Concerns::User
 
+after_create :send_confirmation_email, if: -> { !Rails.env.test? && User.devise_modules.include?(:confirmable) }
 
+private
+def send_confirmation_email
+  self.send_confirmation_instructions
+end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
